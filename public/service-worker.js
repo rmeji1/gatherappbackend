0915759253853ea9b03262d7e7674fee1f142a7e
rmeji1 <rmeji1@unh.newhaven.edu>
2020-01-02 08:54:38 -0500

    added serivce workign using cra-append-sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -37,3 +37,14 @@ workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/i
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
+
+self.addEventListener('push', async event => { //eslint-disable-line
+  const data = event.data.json()
+  if (data.type === 'ADD_INVITATION') {
+    const options = { body: data.invitation.event_title }
+    event.waitUntil(
+      self.registration.showNotification('You got invited!', options) //eslint-disable-line
+    )
+  }
+  self.clients.matchAll().then(clients => clients.map(client => client.postMessage(JSON.stringify(data)))) //eslint-disable-line
+})
